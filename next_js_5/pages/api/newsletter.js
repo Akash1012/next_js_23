2e415f72fa7a63bbecccf2190a8cc6ee1c02735d
Akash1012 <gupta.akash2090@gmail.com>
2023-09-28 23:36:12 +0530

    add error handling

--- next_js_5/pages/api/newsletter.js
+++ next_js_5/pages/api/newsletter.js
@@ -1,5 +1,18 @@
 import { MongoClient } from "mongodb";
 
+async function connectDataBase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://guptaakash2090:8QP11bthmfJYLPmb@cluster0.v18i6r3.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method == "POST") {
     const userEmail = req.body.email;
@@ -10,15 +23,27 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://guptaakash2090:8QP11bthmfJYLPmb@cluster0.v18i6r3.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
-    await db.collection("newsletter").insertOne({
-      email: userEmail,
-    });
+    let client;
+    try {
+      client = await connectDataBase();
+    } catch (error) {
+      res.status(500).json({
+        message: "Connecting to the database failed !",
+      });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, {
+        email: userEmail,
+      });
+      client.close();
+    } catch (error) {
+      res.status(500).json({
+        message: "Inserting data ailed !",
+      });
+      return;
+    }
 
     console.log(userEmail);
     res.status(201).json({
